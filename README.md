2b0e9a8bc9ee143a96987319760cb00e46192570
Ritik Banger <ritikbanger.dev@gmail.com>
2024-02-06 16:13:11 +0530

    added features for react native otp

--- README.md
+++ README.md
@@ -1,2 +1,140 @@
 # react-native-input-otp
-OTP Input for React Native Applications
+
+## Overview
+
+`react-native-input-otp` is a fully customizable, one-time password (OTP) input component designed for React Native applications. This package makes it easy to integrate an OTP input feature into your mobile app, ensuring a user-friendly and secure way for users to input their OTP codes.
+
+## Installation
+
+To install `react-native-input-otp`, run the following command in your React Native project:
+
+```bash
+npm install react-native-input-otp
+```
+
+## Props
+
+The `OtpInput` component accepts several props to customize its behavior and style:
+
+<table>
+  <tr>
+    <th>Prop Name</th>
+    <th>Type</th>
+    <th>Description</th>
+    <th>Default Value</th>
+  </tr>
+  <tr>
+    <td><code>numInputs</code></td>
+    <td><code>number</code></td>
+    <td>The number of input boxes for the OTP.</td>
+    <td><code>4</code></td>
+  </tr>
+  <tr>
+    <td><code>onChange</code></td>
+    <td><code>(otp: string) => void</code></td>
+    <td>Function called with the OTP value when it changes.</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><code>isDisabled</code></td>
+    <td><code>boolean</code></td>
+    <td>If set to <code>true</code>, all input boxes are disabled.</td>
+    <td><code>false</code></td>
+  </tr>
+  <tr>
+    <td><code>autoFocus</code></td>
+    <td><code>boolean</code></td>
+    <td>If set to <code>true</code>, the first input box is focused automatically.</td>
+    <td><code>false</code></td>
+  </tr>
+  <tr>
+    <td><code>keyboardType</code></td>
+    <td><code>KeyboardType</code> (React Native)</td>
+    <td>The type of keyboard to display for inputs.</td>
+    <td><code>'default'</code></td>
+  </tr>
+  <tr>
+    <td><code>value</code></td>
+    <td><code>string</code></td>
+    <td>The current value of the OTP input.</td>
+    <td><code>''</code> (empty string)</td>
+  </tr>
+  <tr>
+    <td><code>containerStyle</code></td>
+    <td><code>ViewStyle</code> (React Native style)</td>
+    <td>Custom styles for the container of the input boxes.</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td><code>inputStyle</code></td>
+    <td><code>TextStyle</code> (React Native style)</td>
+    <td>Custom styles for each input box.</td>
+    <td>-</td>
+  </tr>
+</table>
+
+## Customization
+
+You can customize the appearance of the OTP inputs by providing `containerStyle` and `inputStyle` props. These props accept standard React Native style objects.
+
+## Usage
+
+### In Class Components
+
+First, import the `OtpInput` component from the package:
+
+```javascript
+import OtpInput from 'react-native-input-otp';
+```
+
+Then, you can use the `OtpInput` component in your class component as follows:
+
+```javascript
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import OtpInput from 'react-native-input-otp';
+
+class YourComponent extends Component {
+  handleOtpChange = otp => {
+    console.log(otp); // OTP value
+  };
+
+  render() {
+    return (
+      <View>
+        <OtpInput numInputs={4} onChange={this.handleOtpChange} />
+      </View>
+    );
+  }
+}
+
+export default YourComponent;
+```
+
+### In Functional Components
+
+For functional components, the usage is similar. Import the `OtpInput` component and use it inside your functional component:
+
+```javascript
+import React from 'react';
+import { View } from 'react-native';
+import OtpInput from 'react-native-input-otp';
+
+const YourComponent = () => {
+  const handleOtpChange = otp => {
+    console.log(otp); // OTP value
+  };
+
+  return (
+    <View>
+      <OtpInput numInputs={4} onChange={handleOtpChange} />
+    </View>
+  );
+};
+
+export default YourComponent;
+```
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ index.tsx
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TextInputProps,
+  KeyboardType,
+  TextStyle,
+  ViewStyle,
+} from 'react-native';
+
+interface SingleOtpInputProps extends TextInputProps {
+  index: number;
+  onInputChange: (index: number, text: string) => void;
+  onInputFocus: (index: number) => void;
+  onBackspace: (index: number) => void;
+  inputStyle?: TextStyle;
+  focused?: boolean;
+}
+
+class SingleOtpInput extends Component<SingleOtpInputProps> {
+  private inputRef: React.RefObject<TextInput> = React.createRef();
+
+  componentDidUpdate(prevProps: SingleOtpInputProps) {
+    if (this.props.value !== prevProps.value && this.inputRef.current) {
+      this.inputRef.current.setNativeProps({ text: this.props.value });
+    }
+  }
+
+  handleTextChange = (text: string) => {
+    const { index, onInputChange } = this.props;
+    onInputChange(index, text);
+  };
+
+  handleKeyPress = (e: any) => {
+    if (e.nativeEvent.key === 'Backspace') {
+      const { index, onBackspace } = this.props;
+      onBackspace(index);
+    }
+  };
+
+  render() {
+    const { style, onInputFocus, index, inputStyle, focused, ...rest } =
+      this.props;
+    const combinedStyle = StyleSheet.flatten([
+      styles.input,
+      inputStyle,
+      style,
+      focused ? styles.focusedInput : null,
+    ]);
+
+    return (
+      <TextInput
+        ref={this.inputRef}
+        style={combinedStyle}
+        onChangeText={this.handleTextChange}
+        onKeyPress={this.handleKeyPress}
+        onFocus={() => onInputFocus(index)}
+        {...rest}
+      />
+    );
+  }
+}
+
+interface OtpInputProps {
+  numInputs: number;
+  onChange: (otp: string) => void;
+  isDisabled?: boolean;
+  autoFocus?: boolean;
+  keyboardType?: KeyboardType;
+  value?: string;
+  containerStyle?: ViewStyle;
+  inputStyle?: TextStyle;
+}
+
+interface OtpInputState {
+  otp: string[];
+  focusedInput: number;
+}
+
+class OtpInput extends Component<OtpInputProps, OtpInputState> {
+  static defaultProps = {
+    numInputs: 4,
+    onChange: (otp: string) => console.log(otp),
+    isDisabled: false,
+    autoFocus: false,
+    keyboardType: 'default',
+    value: '',
+  };
+
+  constructor(props: OtpInputProps) {
+    super(props);
+    this.state = {
+      otp: Array(props.numInputs).fill(''),
+      focusedInput: 0,
+    };
+  }
+
+  handleInputChange = (index: number, text: string) => {
+    const newOtp = [...this.state.otp];
+    newOtp[index] = text;
+    this.setState({ otp: newOtp }, () => {
+      this.props.onChange(newOtp.join(''));
+      if (index < this.props.numInputs - 1) {
+        this.focusInput(index + 1);
+      }
+    });
+  };
+
+  focusInput = (inputIndex: number) => {
+    this.setState({ focusedInput: inputIndex });
+  };
+
+  handleBackspace = (index: number) => {
+    if (this.state.otp[index] === '' && index > 0) {
+      this.focusInput(index - 1);
+    }
+  };
+
+  renderInputs = () => {
+    const { numInputs, isDisabled, inputStyle, autoFocus, keyboardType } =
+      this.props;
+    const { otp, focusedInput } = this.state;
+
+    return Array.from({ length: numInputs }, (_, i) => (
+      <SingleOtpInput
+        key={i}
+        index={i}
+        value={otp[i]}
+        onInputChange={this.handleInputChange}
+        onInputFocus={this.focusInput}
+        onBackspace={this.handleBackspace}
+        editable={!isDisabled}
+        autoFocus={autoFocus && i === 0}
+        keyboardType={keyboardType}
+        inputStyle={inputStyle}
+        focused={focusedInput === i}
+        maxLength={1}
+      />
+    ));
+  };
+
+  render() {
+    const { containerStyle } = this.props;
+    return (
+      <View style={[styles.container, containerStyle]}>
+        {this.renderInputs()}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  input: {
+    width: 40,
+    height: 40,
+    margin: 5,
+    textAlign: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+  },
+  focusedInput: {
+    borderColor: 'blue',
+  },
+});
+
+export default OtpInput;
